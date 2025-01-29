8731d0e41a7d2c3a0b78d6d2d5d52445e87920ac
Patrick Meade <blinkdog@protonmail.com>
2025-01-28 20:28:51 -0600

    Did we get all the deps?

--- tools/starfly/ci/build_auxmos.sh
+++ tools/starfly/ci/build_auxmos.sh
@@ -10,12 +10,16 @@ pushd "$HOME/auxmos"
 
 if [ ! -d .git ]
 then
-	git init
-	git remote add origin $AUXMOS_REPO
+    git init
+    git remote add origin $AUXMOS_REPO
 fi
 
 git fetch origin --depth=1 $AUXMOS_VERSION
 git reset --hard FETCH_HEAD
+
+sudo dpkg --add-architecture i386
+sudo apt update || true
+sudo apt install -o APT::Immediate-Configure=false g++-multilib libssl-dev:i386 libgcc-s1:i386 zlib1g-dev:i386
 
 rustup target add i686-unknown-linux-gnu
 
