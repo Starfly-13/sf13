36de09a965bc3688271626d6a70b4b809988afe1
Patrick Meade <blinkdog@protonmail.com>
2025-01-28 20:05:59 -0600

    And now we see the violence inherent in the system

--- tools/starfly/ci/build_windows.ps1
+++ tools/starfly/ci/build_windows.ps1
@@ -1,5 +1,10 @@
 # build_windows.ps1
 
+if(!(Test-Path -Path "C:/byond")){
+    bash tools/starfly/ci/download_byond_windows.sh
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("C:/byond.zip", "C:/")
+}
+
 bash tools/ci/install_node.sh
 bash tools/build/build -Werror
 exit $LASTEXITCODE
--- tools/starfly/ci/download_byond_windows.sh
+++ tools/starfly/ci/download_byond_windows.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# install_byond_windows.sh
+# download_byond_windows.sh
 
 set -euo pipefail
 source dependencies.sh
 if [ ! -f C:/byond.zip ]; then
     curl "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
 fi
-unzip C:/byond.zip
